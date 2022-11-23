6fb5f569a8037bc1f6621339aab9d0aa443d3194
Vanessa Martins <vanymartins2@gmail.com>
2022-11-23 12:46:38 -0300

    Add core components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, quas quo libero labore nemo cumque quos harum vel aut odio possimus id fuga quia, iure at molestias voluptatem exercitationem dolorem.'
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl'
+      ],
+      control: {
+        type: 'inline-radio'
+      }
+    }
+  }
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong'
+  }
+}
